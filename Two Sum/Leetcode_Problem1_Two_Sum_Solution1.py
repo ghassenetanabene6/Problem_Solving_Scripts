857deda2bf1a7d1a36d86d2ba000bc349d2b96d3
Ghassene TANABENE <83358285+ghassenetanabene6@users.noreply.github.com>
2022-01-05 00:05:48 +0100

    Add files via upload

---
+++ Two Sum/Leetcode_Problem1_Two_Sum_Solution1.py
@@ -0,0 +1,36 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        self.nums = nums
+        self.target = target
+        visited_indexes = []
+        elements = []
+
+        for i,v in enumerate(self.nums):
+            if v in self.nums and self.target-v in self.nums:
+                j = self.nums.index(self.target-v)
+                if i != j :
+                    if j in visited_indexes and j<len(self.nums):
+                        try:
+                            k = self.nums.index(self.target-v,j+1)
+                            j = k
+                        except:
+                            pass
+                    visited_indexes.append(j)
+                    if i not in elements and j not in elements:        
+                        elements.append(min(i,j))
+                        elements.append(max(i,j))
+
+        return elements
+
+def main():
+    test_case = Solution()
+    L = [1,2,3,2,4,5,6,7]
+    target = 4
+    print(test_case.twoSum(L, target))
+
+main()
---
+++ Two Sum/Leetcode_Problem1_Two_Sum_Solution2.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+class myarray(np.ndarray):
+    def __new__(cls, *args, **kwargs):
+        return np.array(*args, **kwargs).view(myarray)
+    def index(self, value, start=0):
+        return np.where(self[start::] == value)[0][0]
+    
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        self.nums = myarray(nums)
+        self.target = target
+        visited_indexes = np.array([], dtype=int)
+        elements = np.array([], dtype=int)
+
+        for i,v in enumerate(self.nums):
+            if self.target-v in self.nums:
+                j = self.nums.index(self.target-v)
+                
+                if i != j :
+                    if j in visited_indexes and j<len(self.nums):
+                        try:
+                            k = self.nums.index(self.target-v,j+1)
+                            j = k
+                        except:
+                            pass
+                    visited_indexes = np.append(visited_indexes,j)
+                    if i not in elements and j not in elements:        
+                        elements = np.append(elements, min(i,j))
+                        elements = np.append(elements, max(i,j))
+        return list(elements)
+
+def main():
+    test_case = Solution()
+    L = [1,2,3,2,4,5,6,7]
+    target = 4
+    print(test_case.twoSum(L, target))
+
+main()
