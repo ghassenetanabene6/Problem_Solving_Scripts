41bbfe4629119ba528fa441213ed0064200686bc
Ghassene TANABENE <83358285+ghassenetanabene6@users.noreply.github.com>
2022-01-05 01:50:01 +0100

    Update Leetcode_Problem1_Two_Sum_Solution2.py

--- Two Sum/Leetcode_Problem1_Two_Sum_Solution2.py
+++ Two Sum/Leetcode_Problem1_Two_Sum_Solution2.py
@@ -7,26 +7,25 @@ class myarray(np.ndarray):
         return np.where(self[start::] == value)[0][0]
     
 
-class Solution(object):
+class SolutionForManyValidAnswers(object):
     def twoSum(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
-        self.nums = myarray(nums)
-        self.target = target
+        nums = myarray(nums)
         visited_indexes = np.array([], dtype=int)
         elements = np.array([], dtype=int)
 
-        for i,v in enumerate(self.nums):
-            if self.target-v in self.nums:
-                j = self.nums.index(self.target-v)
+        for i,v in enumerate(nums):
+            if target-v in nums:
+                j = nums.index(target-v)
                 
                 if i != j :
-                    if j in visited_indexes and j<len(self.nums)-1:
+                    if j in visited_indexes and j<len(nums)-1:
                         try:
-                            k = self.nums.index(self.target-v,j+1)
+                            k = nums.index(target-v,j+1)
                             j = k
                         except:
                             pass
@@ -37,7 +36,7 @@ class Solution(object):
         return list(elements)
 
 def main():
-    test_case = Solution()
+    test_case = SolutionForManyValidAnswers()
     L = [1,2,3,2,4,5,6,7]
     target = 4
     print(test_case.twoSum(L, target))
