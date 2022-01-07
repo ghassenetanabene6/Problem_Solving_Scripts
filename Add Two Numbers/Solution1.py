12dcf43c5162550ffd62596c7d4b65816cd7c930
Ghassene TANABENE <83358285+ghassenetanabene6@users.noreply.github.com>
2022-01-07 17:21:05 +0100

    Update Solution1.py

--- Add Two Numbers/Solution1.py
+++ Add Two Numbers/Solution1.py
@@ -1,5 +1,4 @@
 '''
-1568 / 1568 test cases passed.
 Runtime: 111 ms
 Memory Usage: 13.5 MB
 '''
@@ -10,7 +9,31 @@ class ListNode(object):
         self.val = val
         self.next = next
 
+def addTwoNumbers(l1, l2):
+    """
+    :type l1: ListNode
+    :type l2: ListNode
+    :rtype: ListNode
+    """
+    number1 = "" 
+    number2 = ""
+    
+    while l1 or l2:
+        if l1:
+            number1 += str(l1.val)
+            l1 = l1.next
+        if l2:
+            number2 += str(l2.val)
+            l2 = l2.next    
+    res = str(int(number1[::-1]) + int(number2[::-1]))
+    result = ListNode(int(res[0]))
+    if len(res)>1:
+        for i in res[1::]:
+            result = ListNode(int(i), result)
 
+    return result
+
+'''
 def addTwoNumbers(l1, l2):
     """
     :type l1: ListNode
@@ -44,16 +67,14 @@ def addTwoNumbers(l1, l2):
             result = ListNode(int(i), result)
 
     return(result)
-        
+'''
 def main():
     
-    #TestCase
     List1 = ListNode(2,ListNode(4,ListNode(3))) # (2 -> 4 -> 3)
     List2 = ListNode(5,ListNode(6,ListNode(4))) # (5 -> 6 -> 4)
     L1, L2 = List1, List2
     res = addTwoNumbers(L1, L2)
     nodes = []
-    
     while res:
         nodes.append(str(res.val))
         res = res.next
